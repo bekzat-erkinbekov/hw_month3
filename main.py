d4c3554efc1ded8e86940d7e137c30a1dd837bf3
bekzat-erkinbekov <nurbekovich02@gmail.com>
2023-04-05 17:32:40 +0600

    hw5

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from database.bot_db import sql_create
 async def on_startup(_):
     await bot.send_message(chat_id=ADMINS[0],
                            text="Bot started!")
+    await schedule.set_scheduler(
     sql_create()
 
 fsmAdminMentor.register_handlers_mentor(dp)
@@ -14,11 +15,10 @@ fsm_anketa.register_handlers_anketa(dp)
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
-notification.register_handler_notification(dp)
+
 
 extra.register_handlers_extra(dp)
 
-#надо чтоб меньше нагружалось больше загружалось
 
 
 if  __name__ == '__main__':
